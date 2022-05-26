7406e75a1c0bfc3e01d6e9a05d73e7076960a4b7
Triantafyllos Famprikatzis <40291223+TriantafyllosFamprikatzis@users.noreply.github.com>
2022-05-26 15:14:50 +0300

    Complete services module

--- Services and Dependency Injection/services-start/src/app/account/account.component.ts
+++ Services and Dependency Injection/services-start/src/app/account/account.component.ts
@@ -1,18 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AccountsService } from '../account.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  // providers: [LoggingService] 
+  //AccountService must not be provided otherwise we call a new instance and overwrite the old one
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+  // @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  //The type is not optional we need to set the class of service we want
+  constructor(private loggingServ: LoggingService, private accountService: AccountsService) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountService.updateStatus(this.id, status);
+
+    this.accountService.statusUpdated.emit(status);
+
+    // this.statusChanged.emit({id: this.id, newStatus: status});
+
+
+    // console.log('A server status changed, new status: ' + status);
+    // this.loggingServ.logStatusChange(status);
   }
 }
--- Services and Dependency Injection/services-start/src/app/new-account/new-account.component.ts
+++ Services and Dependency Injection/services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,31 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountsService } from '../account.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService] 
+  //AccountService must not be provided otherwise we call a new instance and overwrite the old one
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  //The type is not optional we need to set the class of service we want
+  constructor(private loggingServ: LoggingService, private accountsService: AccountsService ) {
+    this.accountsService.statusUpdated.subscribe((status: string) => alert('New Status: ' + status));
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+
+
+    // console.log('A server status changed, new status: ' + accountStatus);
+    // this.loggingServ.logStatusChange(accountStatus);
   }
 }
