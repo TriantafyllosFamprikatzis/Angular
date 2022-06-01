4d12550a62ea7dc1f7c8685de0c4279e9bae261c
Triantafyllos Famprikatzis <40291223+TriantafyllosFamprikatzis@users.noreply.github.com>
2022-06-01 07:54:28 +0300

    Implement observables in project

--- Project/recipe-book-shopping-list-app/src/app/recipes/recipe.service.ts
+++ Project/recipe-book-shopping-list-app/src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
@@ -6,7 +6,6 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('Tasty Schnitzel', 'A tasty Schnitzel', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', [
