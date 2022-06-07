6910478a1f3676528c4829641f7dbc1e134abfa8
Triantafyllos Famprikatzis <40291223+TriantafyllosFamprikatzis@users.noreply.github.com>
2022-06-07 10:44:56 +0300

    Implement forms in Project

--- Project/recipe-book-shopping-list-app/src/app/recipes/recipe.service.ts
+++ Project/recipe-book-shopping-list-app/src/app/recipes/recipe.service.ts
@@ -1,21 +1,29 @@
-import { Injectable } from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
-import { Recipe } from "./recipe.model";
-
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
-    new Recipe('Tasty Schnitzel', 'A tasty Schnitzel', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', [
-      new Ingredient('Meat', 1),
-      new Ingredient('French Fries', 20)
-    ]),
-    new Recipe('Big Fat Burger', 'What else you need to say?', 'https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646__340.jpg', [
-      new Ingredient('Buns', 2),
-      new Ingredient('Meat', 1)
-    ])
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   constructor(private slService: ShoppingListService) {}
@@ -31,5 +39,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
